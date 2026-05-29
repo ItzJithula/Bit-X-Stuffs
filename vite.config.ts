092e889eb6c0fcea1e1835c14d0f521b21049293
Jithula Bhasitha <jithulabhasitha@proton.me>
2026-05-29 13:13:03 +0530

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     TanStackRouterVite({ autoCodeSplitting: true }),
     tanstackStart({
       server: {
-        preset: "vercel",
+        entry: "src/server.ts",
       },
     }),
     react(),
